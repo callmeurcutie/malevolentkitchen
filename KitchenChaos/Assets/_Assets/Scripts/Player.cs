01aefd7016b5180a553baab4ce937f01e80f6a7a
Dmytro Frolov <d.frolov@human.de>
2025-03-18 12:04:50 +0100

    refactor(GameInput): refactoring game input

--- KitchenChaos/Assets/_Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/_Assets/Scripts/Player.cs
@@ -6,30 +6,17 @@ using UnityEngine;
 
 public class Player:MonoBehaviour 
 {
+	
+	[SerializeField] private float moveSpeed = 8f;
+	[SerializeField] private GameInput gameInput; 
 	float rotateSpeed = 10f;
 	private bool isWalking;
-	[SerializeField] private float moveSpeed = 8f;
 	
+
      private void Update() {
-		Vector2 inputVector = new Vector2(0, 0);
-			if(Input.GetKey(KeyCode.W)){
-			inputVector.y = +1;
-		}
-			if(Input.GetKey(KeyCode.S)){
-			inputVector.y = -1;
-		} 
-			if(Input.GetKey(KeyCode.A)){
-			inputVector.x = -1;
-		} 
-			if(Input.GetKey(KeyCode.D )){
-			inputVector.x = +1;
-		} 
-
-		inputVector = inputVector.normalized;
-
+		Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 		Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 		transform.position += moveDir * moveSpeed * Time.deltaTime;
-
 		
 		isWalking = moveDir != Vector3.zero;
 		transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
