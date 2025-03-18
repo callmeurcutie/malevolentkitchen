35fc428656a74596e921d16e04f8ca26ffdb062e
Dmytro Frolov <d.frolov@human.de>
2025-03-18 13:26:09 +0100

    feat(Player): adding collision using Physics.Raycast

--- KitchenChaos/Assets/_Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/_Assets/Scripts/Player.cs
@@ -15,9 +15,17 @@ public class Player:MonoBehaviour
 
      private void Update() {
 		Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
 		Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+		float playerSize = .7f;
+		bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+	
+		if(canMove){
+
 		transform.position += moveDir * moveSpeed * Time.deltaTime;
-		
+		}		
+
 		isWalking = moveDir != Vector3.zero;
 		transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 }
