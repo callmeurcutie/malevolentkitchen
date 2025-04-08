3d5ae3d191bf5fee0329feacd03ada17014dcf34
Dmytro Frolov <d.frolov@human.de>
2025-04-08 15:12:20 +0200

    feat: selectedCounter visual

--- KitchenChaos2/Assets/_Assets/Scripts/Player.cs
+++ KitchenChaos2/Assets/_Assets/Scripts/Player.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   
+
+ 
     [SerializeField] private GameInput gameInput;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private LayerMask countersLayerMask;
+    
+    private ClearCounter selectedCounter;
+    
+    public EventHandler OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+    }
+
+    public static Player Instance { get; private set; }
+
+    
     
     float rotateSpeed = 10f;
     float playerRadius = .7f;
     float playerHeight = 2f;
     private Vector3 lastInteractDir;
-   
-  private void Update()
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.Log("There is more than one instance of a player");
+        }
+        
+        Instance = this;
+    }
+
+    private void Update()
   {
       HandleMovement();
       HandleInteractions();
@@ -27,23 +50,9 @@ public class Player : MonoBehaviour
 
   private void GameInput_OnInteractAction(object sender, System.EventArgs e)
   {
-        
-      Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-      Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-      if (moveDir != Vector3.zero)
+      if (selectedCounter != null)
       {
-          lastInteractDir = moveDir;
-      }
-
-      float interactDistance = 2f;
-      if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance,countersLayerMask))
-      {
-          if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
-          {
-              //has ClearCounter
-              clearCounter.Interact();
-          }
+          selectedCounter.Interact();
       }
   }
 
@@ -108,10 +117,27 @@ public class Player : MonoBehaviour
           if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
           {
               //has ClearCounter
-             
+              if (clearCounter != selectedCounter)
+              {
+                  SetSelectedCounter(clearCounter);
+              }
           }
+          else
+          {
+              SetSelectedCounter(null);
+          } 
       }
-   
+      else
+      {
+          SetSelectedCounter(null);
+      }
+  }
 
+  private void SetSelectedCounter(ClearCounter selectedCounter)
+  {
+      OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs
+      {
+          selectedCounter = selectedCounter
+      });
   }
 }
