757b13af0a395943288c64909d84f55a5a447b93
Dmytro Frolov <d.frolov@human.de>
2025-04-08 10:48:04 +0200

    feat: collision with objects

--- KitchenChaos2/Assets/_Assets/Scripts/Player.cs
+++ KitchenChaos2/Assets/_Assets/Scripts/Player.cs
@@ -5,30 +5,22 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    
+    float rotateSpeed = 10f;
+    float playerSize = .7f; 
    
   private void Update()
   {
     Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-    
     Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-    transform.position += moveDir * Time.deltaTime * moveSpeed;
+    bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
 
-    float rotateSpeed = 10f;
+    if (canMove)
+    {
+      transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+    
     transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
   } 
-
-
-  private void Awake(){
   
-    if (gameInput == null)
-    {
-      Debug.LogError("GameInput reference is null. Please assign it in the Inspector.");
-    }
-    else
-    {
-      Debug.Log("GameInput reference is set correctly.");
-    }
- 
-  }
-
 }
