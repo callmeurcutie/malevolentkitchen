5b9b8c0933a38897fb8ebbbffe40561945063507
Dmytro Frolov <d.frolov@human.de>
2025-04-08 13:57:11 +0200

    feat: interact event

--- KitchenChaos2/Assets/_Assets/Scripts/GameInput.cs
+++ KitchenChaos2/Assets/_Assets/Scripts/GameInput.cs
@@ -4,19 +4,26 @@ using System;
 public class GameInput : MonoBehaviour
 {
     private PlayerInputActions playerInputActions;
+    public EventHandler OnInteractAction;
     
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Interact.performed += Interact_performed;
     }
 
-    public Vector2 GetMovementVectorNormalized()
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
+        
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+    public Vector2 GetMovementVectorNormalized()
+    { 
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         inputVector = inputVector.normalized;
-
-        Debug.Log(inputVector);
+        
         return inputVector;
     }
 }
